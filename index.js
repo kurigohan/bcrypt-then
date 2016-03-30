eabf1d7731f340b4f159bb75854be4e63dd4b1df
Andy Nguyen <andy.nguyen@sharpnotions.com>
2016-03-30 16:40:24 -0400

    Use bcrypt-nodejs

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 
 var Promise = require('any-promise')
-var bcrypt = require('bcrypt')
+var bcrypt = require('bcrypt-nodejs')
 
 exports.hash = function (password, salt) {
   salt = salt || 10
   return new Promise(function (resolve, reject) {
-    bcrypt.hash(password, salt, function (err, hash) {
-      if (err) return reject(err)
-      resolve(hash)
-    })
+    if (typeof salt !== 'string' && !(salt instanceof String)) {
+      bcrypt.genSalt(salt, function (err, generatedSalt) {
+        bcrypt.hash(password, generatedSalt, null, function (err, hash) {
+          if (err) return reject(err)
+          resolve(hash)
+        })
+      })
+    } else {
+      bcrypt.hash(password, salt, null, function (err, hash) {
+        if (err) return reject(err)
+        resolve(hash)
+      })
+    }
   })
 }
 
